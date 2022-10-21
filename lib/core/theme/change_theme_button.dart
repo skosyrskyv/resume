8ff44fb91f88d673d98362454349a28fbf54c275
skosyrskyv <skosyrskyv@gmail.com>
2022-10-22 03:58:17 +0600

    create change theme button

---
+++ lib/core/theme/change_theme_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:resume/core/theme/bloc/theme_bloc.dart';
+import 'package:resume/core/widgets/app_bar/app_bar_button.dart';
+
+class ChangeThemeButton extends StatelessWidget {
+  const ChangeThemeButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ThemeBloc, ThemeState>(
+      builder: (context, state) {
+        return AppBarButton(
+          child: Icon(Icons.mode_night),
+          onTap: () => context.read<ThemeBloc>().add(
+                state is DarkThemeState
+                    ? const ThemeEvent.setLight()
+                    : const ThemeEvent.setDark(),
+              ),
+        );
+      },
+    );
+  }
+}
