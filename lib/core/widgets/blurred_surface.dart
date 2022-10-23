5d9c741e63325421ef8e71e749fed5dc7cfe6af2
skosyrskyv <skosyrskyv@gmail.com>
2022-10-23 22:25:18 +0600

    add blurred surface

---
+++ lib/core/widgets/blurred_surface.dart
@@ -0,0 +1,50 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:resume/utils/empty.dart';
+
+class BlurredSurface extends StatelessWidget {
+  final double? height;
+  final double? width;
+  final EdgeInsets margin;
+  final BorderRadius borderRadius;
+  final Color? background;
+  final double sigmaX;
+  final double sigmaY;
+  final Widget child;
+  const BlurredSurface({
+    super.key,
+    this.height,
+    this.width,
+    this.margin = EdgeInsets.zero,
+    this.borderRadius = BorderRadius.zero,
+    this.background,
+    this.sigmaX = 10,
+    this.sigmaY = 10,
+    this.child = const EmptyWidget(),
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: margin,
+      child: ClipRRect(
+        borderRadius: borderRadius,
+        child: BackdropFilter(
+          filter: ImageFilter.blur(
+            sigmaX: sigmaX,
+            sigmaY: sigmaY,
+          ),
+          child: Container(
+            height: height,
+            width: width,
+            decoration: BoxDecoration(
+              color: background,
+            ),
+            child: child,
+          ),
+        ),
+      ),
+    );
+  }
+}
