0422cc6b9aa5a9d2bfcb9025aeac8f50d8ff192f
skosyrskyv <skosyrskyv@gmail.com>
2022-11-12 08:06:03 +0600

    Create platform_logo_asset_keys.dart

---
+++ lib/constants/platform_logo_asset_keys.dart
@@ -0,0 +1,7 @@
+import 'package:resume/features/projects/data/models/platform_names.dart';
+
+const kPlatformLogoAssetKeys = <PlatformName, String>{
+  PlatformName.Android: 'assets/images/android_logo.png',
+  PlatformName.IOS: 'assets/images/ios_logo.png',
+  PlatformName.Web: 'assets/images/web_logo.png',
+};
