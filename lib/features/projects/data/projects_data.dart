dd3a2e33327599b75764ceb385129a82e19b51e1
skosyrskyv <skosyrskyv@gmail.com>
2022-11-13 16:52:32 +0600

    update projects data

--- lib/features/projects/data/projects_data.dart
+++ lib/features/projects/data/projects_data.dart
@@ -3,44 +3,52 @@ import 'package:resume/features/projects/data/models/project_model.dart';
 import 'package:resume/features/projects/data/models/source_link.dart';
 
 const kProjectsData = <ProjectModel>[
-  ProjectModel(
-    name: 'resume',
-    info: 'resume_project_info',
-    asset: '',
-    links: [],
-  ),
-  ProjectModel(
-    name: 'gogo_courier',
-    info: 'gogo_courier_project_info',
-    asset: 'assets/images/pm_screens.png',
-    links: [],
-  ),
-  ProjectModel(
-    name: 'gogo_partner',
-    info: 'gogo_partner_project_info',
-    asset: 'assets/images/pm_screens.png',
-    links: [],
-  ),
+  // ProjectModel(
+  //   name: 'resume',
+  //   info: 'resume_project_info',
+  //   asset: '',
+  //   links: [],
+  // ),
+  // ProjectModel(
+  //   name: 'gogo_courier',
+  //   info: 'gogo_courier_project_info',
+  //   asset: 'assets/images/pm_screens.png',
+  //   links: [],
+  // ),
+  // ProjectModel(
+  //   name: 'gogo_partner',
+  //   info: 'gogo_partner_project_info',
+  //   asset: 'assets/images/pm_screens.png',
+  //   links: [],
+  // ),
   ProjectModel(
     name: 'mondino_tracker',
     info: 'mondino_tracker_project_info',
     asset: 'assets/images/pm_screens.png',
     links: [
-      SourceLink(url: 'https://google.com/', platform: PlatformName.IOS),
-      SourceLink(url: 'https://google.com/', platform: PlatformName.Android),
-      SourceLink(url: 'https://google.com/', platform: PlatformName.Web),
+      SourceLink(
+          url:
+              'https://apps.apple.com/us/app/мондино-трекер-здоровья/id1582953573',
+          platform: PlatformName.IOS),
+      SourceLink(
+          url:
+              'https://play.google.com/store/apps/details?id=com.mondino.pocketmedic',
+          platform: PlatformName.Android),
+      SourceLink(
+          url: 'https://skosyrskyv.github.io/resume/',
+          platform: PlatformName.Web),
     ],
   ),
-  ProjectModel(
-    name: 'mondino_tracker_doctor',
-    info: 'mondino_tracker_doctor_project_info',
-    asset: 'assets/images/pm_screens.png',
-    links: [],
-  ),
-  ProjectModel(
-    name: 'osher_chat_ui',
-    info: 'osher_chat_ui_project_info',
-    asset: 'assets/images/pm_screens.png',
-    links: [],
-  ),
+  // ProjectModel(
+  //   name: 'mondino_tracker_doctor',
+  //   info: 'mondino_tracker_doctor_project_info',
+  //   asset: 'assets/images/pm_screens.png',
+  //   links: [],
+  // ),
+  // ProjectModel(
+  //   name: 'osher_chat_ui',
+  //   info: 'osher_chat_ui_project_info',
+  //   asset: 'assets/images/pm_screens.png',
+  //   links: [],
+  // ),
 ];
