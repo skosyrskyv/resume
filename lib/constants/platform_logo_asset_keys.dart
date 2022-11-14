890b39f7d87c91369f123d47efc55672f293854b
skosyrskyv <skosyrskyv@gmail.com>
2022-11-14 11:12:43 +0600

    add new logos

--- lib/constants/platform_logo_asset_keys.dart
+++ lib/constants/platform_logo_asset_keys.dart
@@ -4,4 +4,8 @@ const kPlatformLogoAssetKeys = <PlatformName, String>{
   PlatformName.Android: 'assets/images/android_logo.png',
   PlatformName.IOS: 'assets/images/ios_logo.png',
   PlatformName.Web: 'assets/images/web_logo.png',
+  PlatformName.Github: 'assets/images/github_logo.png',
+  PlatformName.MacOS: 'assets/images/macos_logo.png',
+  PlatformName.Windows: 'assets/images/windows_logo.png',
+  PlatformName.Linux: 'assets/images/linux_logo.png',
 };
